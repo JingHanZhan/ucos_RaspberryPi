16ca9204971ec346558c443de05f406c1c317b36
Your Name <you@example.com>
2019-03-16 17:38:15 +0800

    Add mailbox example, use 'make TASK=mailbox' to compile

--- usrApp/mailbox/main.c
+++ usrApp/mailbox/main.c
@@ -5,39 +5,45 @@
 
 OS_STK userAppTaskStk1[1000];
 OS_STK userAppTaskStk2[1000];
+OS_STK userAppTaskStk3[1000];
 OS_STK userAppTaskStk[1000];
 
 extern void userApp1(void *);
 extern void userApp2(void *);
 extern void uartRS(void *);
+extern void createMbox(void *);
 
 void main() {
-    uart_init();
-    uart_string("uart_init() success!");
+  uart_init();
+  uart_string("uart_init() success!");
 
-    InitInterruptController();
-    uart_string("InitInterruptController success!");
+  InitInterruptController();
+  uart_string("InitInterruptController success!");
 
-    DisableInterrupts();
-	uart_string("DisableInterrupts() success!");
+  DisableInterrupts();
+  uart_string("DisableInterrupts() success!");
 
-    timer_init();
-	uart_string("timer_init() success!");
+  timer_init();
+  uart_string("timer_init() success!");
 
-    OSInit();
-	uart_string("OSInit() success!");
+  OSInit();
+  uart_string("OSInit() success!");
 
-    OSTaskCreate(userApp1, (void *)0, &userAppTaskStk1[1000 - 1], 5);
-	uart_string("Task userApp1 creat success!");
+  OSTaskCreate(createMbox, (void *)0, &userAppTaskStk3[1000 - 1], 5);
+  uart_string("Task createMbox creat success!");
 
-    OSTaskCreate(userApp2, (void *)0, &userAppTaskStk2[1000 - 1], 6);
-	uart_string("Task userApp2 creat success!");
+  OSTaskCreate(userApp1, (void *)0, &userAppTaskStk1[1000 - 1], 6);
+  uart_string("Task userApp1 creat success!");
 
-	OSTaskCreate(uartRS, (void *)0, &userAppTaskStk[1000 - 1], 7);
-	uart_string("Task uartRS creat success!");
+  OSTaskCreate(userApp2, (void *)0, &userAppTaskStk2[1000 - 1], 7);
+  uart_string("Task userApp2 creat success!");
 
-    OSStart();
-	uart_string("OSStart() success!");
+  OSTaskCreate(uartRS, (void *)0, &userAppTaskStk[1000 - 1], 8);
+  uart_string("Task uartRS creat success!");
 
-    while (1);
+  OSStart();
+  uart_string("OSStart() success!");
+
+  while (1)
+    ;
 }
--- usrApp/mailbox/userApp.c
+++ usrApp/mailbox/userApp.c
@@ -5,18 +5,31 @@ extern void PUT32(unsigned int, unsigned int);
 extern unsigned int GET32(unsigned int);
 #define GPSETO 0x3F20001C
 #define GPCLR0 0x3F200028
+OS_EVENT *Mbox1;
 
 void userApp2(void *args) {
+  char *Msg;
+  INT8U err;
+
   while (1) {
     // PUT32(GPSETO, 1 << 16);
     uart_string("in userApp2");
+    // Msg = (char *)OSMboxPend(Mbox1, 0, &err);
+    Msg = (char *)OSMboxAccept(Mbox1);
+    uart_string(Msg);
     OSTimeDly(100);
   }
 }
 
 void userApp1(void *args) {
+  char txmsg = 'A';
   while (1) {
     uart_string("in userApp1");
+    OSMboxPost(Mbox1, (void *)&txmsg);
+    txmsg++;
+    if (txmsg == 'Z') {
+      txmsg = 'A'; /* Start new series of messages                       */
+    }
     OSTimeDly(100);
     // PUT32(GPCLR0, 1 << 16);
   }
@@ -25,6 +38,15 @@ void userApp1(void *args) {
 void uartRS(void *args) {
   while (1) {
     uart_send(uart_recv());
+  }
+}
+
+void createMbox(void *args) {
+  uart_string("enter createMbox");
+  Mbox1 = OSMboxCreate((void *)0);
+  uart_string("Create first mailbox name Mbox1");
+  while (1) {
+    OSTimeDly(100);
   }
 }
 
